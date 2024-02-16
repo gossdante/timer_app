f31f8c3e27d35fb8efe9732ebfea83933351bfb5
Dante Goss <ddg1210@gmail.com>
2024-02-16 10:02:43 -0500

    reordered

--- timer_app.py
+++ timer_app.py
@@ -9,7 +9,33 @@ st.title('5 Minute Timer with Sequential Messages & Calculators')
 st.write('This app will display a timer counting down from 5 minutes. With two minutes left it will say to "Add Marker in Omnia", at one minute left it will say to "Get RPE and Awkwardness", and at 30 seconds left it will say to "Start Recording in MotionMonitor". At the bottom there are calculators that will take number of steps taken in 15 seconds to step rate per minute, and calcualte 105% and 110% of that value. 90% and 110% of preferred gait speed will also be calculated.')
 st.write('This is meant as a guide for data collection for the CADENCE Study.')
 st.write('---')
+st.title('Calculators')
+gs = st.number_input("Input the preferred gait speed value.")
+if gs >0:
+    below = round(gs*.9,2)
+    above = round(gs*1.1,2)
+    cooldown = round(gs*.6,2)
+    column1,column2,column3,column4 = st.columns(4)
+    column1.metric('10% Below PGS',str(below))
+    column2.metric('PGS',str(gs))
+    column3.metric('10% Above PGS',str(above))
+    column4.metric('60% Of PGS (Easy Walking)',str(cooldown))
 
+numval = st.number_input("Input the number of steps taken in 15 seconds")
+if numval >1:
+    full = round(numval*4)
+    sixty = round((numval*.6*4))
+    onefive = round((numval*1.05*4))
+    oneone = round((numval*1.1*4))
+
+    col2,col3,col4 = st.columns(3)
+    col2.metric('Step Rate: 100%',str(full))
+    col3.metric('Step Rate: 105%',str(onefive))
+    col4.metric('Step Rate: 110%',str(oneone))
+
+
+st.write('---')
+st.title('Timer')
 # Create a start button
 if st.button('Start Timer'):
 
@@ -52,30 +78,5 @@ if st.button('Start Timer'):
     # Once the countdown is over, display the message
     timer_placeholder.markdown("### Timer Ended! On to the next trial!")
 st.write('---')
-st.write('Calculators')
-gs = st.number_input("Input the preferred gait speed value.")
-if gs >0:
-    below = round(gs*.9,2)
-    above = round(gs*1.1,2)
-    cooldown = round(gs*.6,2)
-    column1,column2,column3,column4 = st.columns(4)
-    column1.metric('10% Below PGS',str(below))
-    column2.metric('PGS',str(gs))
-    column3.metric('10% Above PGS',str(above))
-    column4.metric('60% Of PGS (Easy Walking)',str(cooldown))
 
-numval = st.number_input("Input the number of steps taken in 15 seconds")
-if numval >1:
-    full = round(numval*4)
-    sixty = round((numval*.6*4))
-    onefive = round((numval*1.05*4))
-    oneone = round((numval*1.1*4))
-
-    col2,col3,col4 = st.columns(3)
-    col2.metric('Step Rate: 100%',str(full))
-    col3.metric('Step Rate: 105%',str(onefive))
-    col4.metric('Step Rate: 110%',str(oneone))
-
-
-st.write('---')
 st.write('Written by Dante Goss')
